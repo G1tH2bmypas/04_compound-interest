525e0e9a0bc0775ed38f5a24246b20e9fcd09517
Tekyto <Tekyto2014@gmail.com>
2022-10-13 22:28:56 +0300

    Dyad'ky , ya zrobyv :p

--- index.cpp
+++ index.cpp
@@ -3,5 +3,10 @@
 #include <cmath>
 
 long compoundInterest(long startDeposit, double annualRate, int annualAmount, int interestPeriod) {
-	return 0;
+//	double division = annualRate / annualAmount;
+//	double sum = division + 1;
+//	int dob = annualAmount * interestPeriod;
+//	double degree = pow(sum, dob);
+//	double A = startDeposit * degree;
+	return round(startDeposit * (pow (1 + annualRate / annualAmount, annualAmount * interestPeriod)));
 }
